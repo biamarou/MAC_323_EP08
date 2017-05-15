f268b4a5d796f87d10156b10e33c6466e90daa64
leolanavo <leo0198@gmail.com>
2017-05-14 21:21:51 -0300

    PointST is working

--- PointST.java
+++ PointST.java
@@ -2,7 +2,10 @@ import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.In;
 import java.lang.Iterable;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class PointST<Value> {
     RedBlackBST<Point2D, Value> points;
@@ -20,14 +23,20 @@ public class PointST<Value> {
     } // number of points 
 
     public void put(Point2D p, Value val) {
+	if (p == null || val == null)
+	    throw new java.lang.NullPointerException();
 	points.put(p, val);
     } // associate the value val with point p
 
     public Value get(Point2D p) {
+	if (p == null)
+	    throw new java.lang.NullPointerException();
 	return points.get(p);
     } // value associated with point p 
 
     public boolean contains(Point2D p) {
+	if (p == null)
+	    throw new java.lang.NullPointerException();
 	return points.contains(p);
     } // does the symbol table contain point p? 
 
@@ -36,6 +45,9 @@ public class PointST<Value> {
     } // all points in the symbol table 
 
     public Iterable<Point2D> range(RectHV rect) {
+	if (rect == null)
+	    throw new java.lang.NullPointerException();
+	
 	Iterable<Point2D> STpoints = points();
 	Bag<Point2D> inside = new Bag<Point2D>();
 	for (Point2D p : STpoints)
@@ -45,6 +57,9 @@ public class PointST<Value> {
     } // all points that are inside the rectangle 
 
     public Point2D nearest(Point2D p) {
+	if (p == null)
+	    throw new java.lang.NullPointerException();
+
 	if (points.isEmpty()) {
 	    return null;
 	}
@@ -52,12 +67,57 @@ public class PointST<Value> {
 	Iterable<Point2D> allPoints = points();
 	for (Point2D pi : allPoints)
 	    if (near == null || near.distanceSquaredTo(p) > pi.distanceSquaredTo(p))
-		near = pi;
+	        near = pi;
 	return near;    
+
+    } // a nearest neighbor to point p; null if the symbol table is empty
+
+    public Iterable<Point2D> nearest(Point2D p, int k) {
+	if (p == null)
+	    throw new java.lang.NullPointerException();
+	
+	if (points.isEmpty()) {
+	    return null;
+	}
+	Point2D near; 
+	Iterable<Point2D> allPoints = points();
+	LinkedList<Point2D> Knearest = new LinkedList<>();
+
+	if (points.size() <= k)
+	    return allPoints;
+	
+	while (k > 0) {
+	    near = null;
+	    for (Point2D pi : allPoints)
+		if ((near == null || near.distanceSquaredTo(p) > pi.distanceSquaredTo(p)))
+		    if (!Knearest.contains(pi))
+			near = pi;
+	    Knearest.add(near);
+	    k--;
+	}
+
+	return Knearest;    
 
     } // a nearest neighbor to point p; null if the symbol table is empty 
 
     public static void main(String[] args) {
+
+	In file = new In(args[0]);
+	PointST<Integer> arvore = new PointST<Integer>();
+	Point2D ponto;
+	Integer i = 0;
+	
+	while(!file.isEmpty()) {
+	    ponto = new Point2D(file.readDouble(), file.readDouble());
+	    arvore.put(ponto, i);
+	    i++;
+	    Iterable<Point2D> iteravel = arvore.nearest(ponto, 2);
+	    System.out.println("Near" + arvore.nearest(ponto));
+	    System.out.println("Ponto " + ponto);
+	    for (Point2D p : iteravel) {
+		System.out.println(p);
+	    }
+	}
 
     } // unit testing (required) 
 }
